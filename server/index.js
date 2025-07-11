e35c4101dd346bf3b5bed0c5d41e35cae332ff54
Pugaz2006 <pugaz2006@gmail.com>
2025-07-11 15:56:17 +0530

    last and final

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use("/api/news", newsRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
